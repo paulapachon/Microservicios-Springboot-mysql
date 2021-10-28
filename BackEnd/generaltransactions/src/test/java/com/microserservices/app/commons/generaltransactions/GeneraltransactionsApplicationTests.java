30df052ddde2363eb58dca127b7dcf6bdb5f4b4a
paulapachon <ppachonr@correo.udistrital.edu.co>
2021-10-28 14:50:58 -0500

    Subiendo backend

---
+++ BackEnd/generaltransactions/src/test/java/com/microserservices/app/commons/generaltransactions/GeneraltransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserservices.app.commons.generaltransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneraltransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
